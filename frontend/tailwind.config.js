ed09a9b549721dd4073e5e822445316b99b43e36
Jazlan06 <jazlannaik4@gmail.com>
2025-10-14 16:52:43 +0530

    Switched from React to NextJS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
   ],
   theme: {
     extend: {
       fontFamily: {
-        cormorant: ['"Cormorant Garamond"', 'serif'], // Added elegant serif font
+        primary: ['Inter', 'sans-serif'],
+        heading: ['Playfair Display', 'serif']
       },
+      colors: {
+        primary: '#121212',
+        accent: '#f4a261',
+        soft: '#fefae0',
+      }
     },
   },
   plugins: [],
